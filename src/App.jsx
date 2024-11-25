3d0b91d35d76a90d2f1ec9e8d8d74bd916eaf14e
Neha <neha_239135@saitm.ac.in>
2024-11-25 17:42:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Textform from './Components/Textform';
+import Alert from './Components/Alert';
+
+function App() {
+  return (
+    <>
+      <Alert alert={alert}/>
+      <Textform heading="head" />
+    </>
+
+
+  );
+}
+
+export default App;
